30f940705cb1a246a6f210ad02b2ef86634d3b5b
TessShearer <shearer.tess@gmail.com>
2022-11-02 17:08:39 -0600

    added images and css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Resume from './components/Resume';
+// import Contact from './components/Contact';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+
+  const [selectedSection, setSelectedSection] = useState("About Me")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <header className="header">
+            <h1>
+                Tess Shearer
+            </h1>
+            <nav>
+                <ul>
+                    <li className={(selectedSection)==="About Me"?"selected navLink":"navLink"} onClick={() => setSelectedSection("About Me")}>
+                        About Me
+                    </li>
+                    <li className={(selectedSection)==="Portfolio"?"selected navLink":"navLink"} onClick={() => setSelectedSection("Portfolio")}>
+                        Portfolio
+                    </li>
+                    <li className={(selectedSection)==="Resume"?"selected navLink":"navLink"} onClick={() => setSelectedSection("Resume")}>
+                        Resume
+                    </li>
+                    {/* <li className={(selectedSection)==="Contact"?"selected navLink":"navLink"} onClick={() => setSelectedSection("Contact")}>
+                        Contact Me
+                    </li> */}
+                </ul>
+            </nav>
+        </header>
+      <section className="App">
+        <main>
+          {selectedSection === "About Me" && <About></About>}
+          {selectedSection === "Portfolio" && <Portfolio></Portfolio>}
+          {selectedSection === "Resume" && <Resume></Resume>}
+          {/* {selectedSection === "Contact" && <Contact></Contact>} */}
+        </main>
+      </section>
+      <section className="footer">
+<a href="https://github.com/TessShearer">GitHub</a>
+<a href="https://www.linkedin.com/in/tess-shearer-a343b2232/">LinkedIn</a>
+<a href="https://stackoverflow.com/users/18235820/tess-shearer?tab=profile">Stack Overflow</a>
+      </section>
     </div>
   );
 }
